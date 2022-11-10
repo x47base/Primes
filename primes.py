b988fbb7388488f191d3caa8e06fd8a2278d6734
x47 <72315013+x47base@users.noreply.github.com>
2022-11-10 20:30:30 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,11 +3,8 @@ def main():
     sieve = list(range(0))
     start_limit = int(input("Start Limit: "))
     end_limit = int(input("End Limit: "))
-
-    sieve = list(range(0, end_limit+1)) # von 1 bis zur oberen Limite führt dazu das die 0 fehlt und der Test fehlschlägt.
-
+    sieve = list(range(0, end_limit+1)) 
     prime = 2
-
     while prime <= end_limit:
         if sieve[prime] > 0:
             if prime >= start_limit:
@@ -16,11 +13,8 @@ def main():
             while multiples_of_the_number <= end_limit:
                 sieve[multiples_of_the_number] = 0
                 multiples_of_the_number += prime
-
         prime += 1
-
-    return sieve            # Diese Zeile muss als letzte Zeile in main stehen bleiben
-
+    return sieve
 
 if __name__ == '__main__':
     main()
